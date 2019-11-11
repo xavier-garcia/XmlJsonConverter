6c0090414ad5d5699555d409d094b8a73e53ccc2
xgarcia <xgarcia@voxelgroup.net>
2019-11-11 10:36:19 +0100

    first commit

---
+++ XmlToJson/Converters/XmlConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace XmlToJson.Converters
+{
+    public static class XmlConverter
+    {
+        private const string jsonPath = @"C:\katas\XmlToJson\XmlToJson\Json\";
+        private const string xmlPath = @"C:\katas\XmlToJson\XmlToJson\Xml\";
+
+        public static void Process()
+        {
+            try
+            {
+                var sources = new List<string>()
+                {
+                    "6262109572_CareOf_True_UT.json",
+                    "6262109573_CareOf_True_UT.json",
+                    "6262109574_CareOf_False_UT.json"
+                };
+
+                foreach (var document in sources)
+                {
+                    var xmlDocument = XmlDocumentCreator(document);
+                    SaveXmlDocument(xmlDocument, document);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        private static XmlDocument XmlDocumentCreator(string document)
+        {
+            var path = Path.Combine(jsonPath, document);
+            var jsonContent = File.ReadAllText(path);
+
+            var node = JsonConvert.DeserializeXNode(jsonContent, "Root");
+
+            return new XmlDocument {InnerXml = node.ToString()};
+        }
+
+        private static void SaveXmlDocument(XmlDocument xmlDocument, string document)
+        {
+            var documentName = document.Split('.')[0];
+
+            xmlDocument.Save($"{xmlPath}{documentName}.xml");
+        }
+    }
+}
---
+++ XmlToJson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using XmlToJson.Converters;
+
+namespace XmlToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an option:\n\n1. JSON To XML\n");
+            var option = Console.ReadKey();
+
+            if (option.KeyChar != '2') return;
+
+            XmlConverter.Process();
+            Console.WriteLine("\nConverting JSON To XML...\n");
+        }
+    }
+}
